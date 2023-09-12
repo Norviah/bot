c1d985f6828b91ce8c51b1e9c977c4f42fb7f804
norviah <21983700+Norviah@users.noreply.github.com>
2023-09-12 09:30:17 -0700

    feat(structs/ModuleUtil): implement a method to join strings separated by a word

--- src/structs/ModuleUtil.ts
+++ src/structs/ModuleUtil.ts
@@ -43,4 +43,32 @@ export class ModuleUtil {
 
     return `${interaction.guild.name}${interaction.channel ? `#${interaction.channel.name}` : ''} [${interaction.guild.id}]`;
   }
+
+  /**
+   * Converts the provided array into a string, with the last item separated by
+   * the specified word.
+   *
+   * If we want to convert an array to a string, we use the `join` method,
+   * however, this will join all items in the array with the specified
+   * separator. It isn't possible to separate the last item with a different
+   * separator using the built-in `join` method.
+   *
+   * This method will convert the array to a string, with the last item
+   * separated by the specified word to simplify the process of converting an
+   * array to a string.
+   *
+   * @param array The array to convert to a string.
+   * @param word The word to separate the last item with.
+   * @example
+   * ```ts
+   * util.join(['a', 'b', 'c'], 'and');
+   * // => a, b and c
+   *
+   * util.join(['a'], 'or');
+   * // => a
+   * ```
+   */
+  public join(array: unknown[], word: string): string {
+    return array.join(', ').replace(/, ?(?:[^,]*)$/, ` ${word} $1`);
+  }
 }
